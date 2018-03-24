2870ae65e7cb77a890567c19becbbb99bdb99c51
shashidharrama <35594160+shashidharrama@users.noreply.github.com>
2018-03-24 11:35:07 +0530

    Create search.sh

---
+++ search.sh
@@ -0,0 +1,9 @@
+read -p "Enter a filename: " filename
+
+if [[ -f $filename ]] ; then
+	    echo "The file $filename exists."
+	        read -p "Enter the word you want to find: " word
+		    grep "$word" "$filename"
+	    else
+		        echo "The file $filename does not exist."
+		fi
